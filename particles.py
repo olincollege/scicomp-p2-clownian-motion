19b1e5fafd7641942ec40b086fba7803454b3952
Aydin O'Leary <aydinoleary@gmail.com>
2023-02-18 16:44:10 -0500

    separate position and velocity vectors

--- particles.py
+++ particles.py
@@ -1,20 +1,20 @@
 import numpy as np
+from scipy import spatial as sp
 import pandas as pd
 
 class Particles:
     def __init__(self, xsize, ysize):
-        self.particles = pd.DataFrame(columns=['n', 'r', 'p', 'v'])
+        self.particles = pd.DataFrame(columns=['n', 'r', 'px', 'py', 'vx', 'vy'])
         self.xlim = (-xsize/2, xsize/2)
         self.ylim = (-ysize/2, ysize/2)
 
     def add_particle(self, tag, r):
-        new_position = np.array([0, 0])
-        new_velocity = np.array([1, 1])
-        new_particle = {'n': tag, 'r': r, 'p': [new_position], 'v': [new_velocity]}
-        self.particles = pd.concat([self.particles, pd.DataFrame(new_particle)])
+        new_particle = {'n': tag, 'r': r, 'px': 0, 'py': 0, 'vx': 1, 'vy': 1}
+        self.particles = pd.concat([self.particles, pd.DataFrame(new_particle, index=[0])])
 
     def move_particles(self, timestep):
-        self.particles['p'] += self.particles['v'] * timestep
+        self.particles['px'] += self.particles['vx'] * timestep
+        self.particles['py'] += self.particles['vy'] * timestep
 
 
 
@@ -25,4 +25,3 @@ if __name__ == "__main__":
     pars.add_particle(1, 1)
     pars.move_particles(0.5)
     print(pars.particles)
-
