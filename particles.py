d553094943e5932a0f1ae10686cda05ea4a2a147
Aydin O'Leary <aydinoleary@gmail.com>
2023-02-26 16:43:20 -0500

    condense wall-collision into smaller array operations

--- particles.py
+++ particles.py
@@ -4,39 +4,41 @@ import pandas as pd
 
 class Particles:
     def __init__(self, xsize, ysize):
+        # name, radius, x position, y position, x velocity, y velocity
         self.particles = pd.DataFrame(columns=['n', 'r', 'px', 'py', 'vx', 'vy'])
+        # center box on (0,0)
         self.xlim = xsize/2
         self.ylim = ysize/2
 
     def add_particle(self, tag, r):
-        # separate x and y coordinates
-        #   doubles # of matrix operations
-        #   BUT makes it more ergonomic to check neighbors &tc.
         new_particle = {'n': tag, 'r': r, 'px': -3.9, 'py': 0, 'vx': 1, 'vy': 1}
+
+        # there may be a better way to do this?
         self.particles = pd.concat([self.particles, pd.DataFrame(new_particle, index=[0])])
 
     def move_particles(self, timestep):
-        # move particles horizontally
-        self.particles['px'] += self.particles['vx'] * timestep
-        # check for horizontal overlaps with the bounding box
-        x_overlap = (abs(self.particles['px']) + self.particles['r']) >= self.xlim
-        # change velocity vector of horizontal overlaps
-        self.particles['vx'].loc[x_overlap] *= -1
-        # change position to correct post-collision (a posteriori)
-        self.particles['px'].loc[x_overlap] += 2 * self.particles['vx'].loc[x_overlap] * timestep
+        pos = self.particles[['px','py']].to_numpy()
+        vel = self.particles[['vx','vy']].to_numpy()
+        rad = self.particles['r'].to_numpy()
 
-        # same process for the y-axis
-        self.particles['py'] += self.particles['vy'] * timestep
-        y_overlap = (abs(self.particles['py']) + self.particles['r']) >= self.ylim
-        self.particles['vy'].loc[y_overlap] *= -1
-        self.particles['py'].loc[y_overlap] += 2 * self.particles['vy'].loc[y_overlap] * timestep
+        pos += vel * timestep
 
+        # create two overlap columns 
+        overlap = (abs(pos) + rad) >= [self.xlim, self.ylim]
 
-        # notes for adding inter-particle collision
-        # project the points across several axes (x=0, y=0, y=x, y=-x?)
-        # AND each axis to get every particle that is potentially colliding
-        # and then brute-force among those
+        # take advantage of True == 1 & False == 0
+        # (-2 * overlap + 1) converts True, False into Negative, Positive
+        # so if overlap == True, that vector is inverted
+        vel *= (-2 * overlap + 1)
 
+        # a posteriori move particles to where they should be post-collision
+        pos += 2 * overlap * vel * timestep
+
+        # ensure the particles array holds the new pos/vel values
+        # to_numpy() does not guarantee that it returns a view rather than a copy
+        self.particles[['px', 'py']] = pos
+        self.particles[['vx', 'vy']] = vel
+        
 if __name__ == "__main__":
     # initialize a default 10x10 array, center of (0,0)
     pars = Particles(10, 10)
@@ -44,10 +46,9 @@ if __name__ == "__main__":
     # do all the particle adding here
     pars.add_particle(0, 1)
 
-    # print the initial state
     # essential for getting the headers to plot
     print(f"time,{','.join(map(str,pars.particles.columns.to_list()))}")
     
-    for i in range(0, 100):
+    for i in range(0, 50):
         print(f"{i},{pars.particles.to_csv(index=False, header=False)}".strip())
         pars.move_particles(0.1)
